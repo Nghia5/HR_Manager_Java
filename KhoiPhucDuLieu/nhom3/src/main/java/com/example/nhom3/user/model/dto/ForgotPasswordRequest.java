fab95fa232cd6554b3231d335332d8e4ed32fcc9
Nghia5 <nghiaak0909@gmail.com>
2026-05-07 21:44:59 +0700

    Xoa folder thua

--- KhoiPhucDuLieu/nhom3/src/main/java/com/example/nhom3/user/model/dto/ForgotPasswordRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.nhom3.user.model.dto;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
-public record ForgotPasswordRequest(
-        @NotBlank @Email String email) {
-}
--- KhoiPhucDuLieu/nhom3/src/main/java/com/example/nhom3/user/model/dto/ResetPasswordRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.nhom3.user.model.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
-public record ResetPasswordRequest(
-        @NotBlank String token,
-        @NotBlank @Size(min = 6) String newPassword) {
-}
